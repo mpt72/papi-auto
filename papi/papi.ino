385e874b4c89f8455c0b10cb11d4ed06a937df2e
Peter Molnar <peter.molnar@nokia.com>
2024-04-23 19:09:41 +0200

    Final version

--- papi/papi.ino
+++ papi/papi.ino
@@ -1,5 +1,5 @@
 #define CLASSES   5
-#define CLASS1    3   // class1 stars, mosfet
+#define CLASS1    3   // class1 stars, mosfet                    
 #define CLASS2    5   // class2 stars, W/O mosfet (only one led)
 #define CLASS3    6   // class3 stars, mosfet
 #define CLASS4    9   // class4 stars, mosfet
@@ -13,13 +13,15 @@
 #define MODE_STAR        1
 #define MODE_TWINKLE     2
 #define MODE_LAMP        3
-#define MODE_FULL_LANP   4
-#define _MODE_END        5
+#define _MODE_END        4
 
 #define TWINKLE   5
 
+#define MAXVALUE 224
+#define MINVALUE 32
+
 int pins[]       = {CLASS1, CLASS2, CLASS3, CLASS4, CLASS5};
-int brightness[] = {255, 224, 192, 158, 126};
+int brightness[] = {192,  224,  96, 64, 32};
 int off[]        = {0, 0, 0, 0, 0};
 int current[]    = {0, 0, 0, 0, 0};
 
@@ -28,7 +30,10 @@ int mode = MODE_OFF;
 bool prevStatus = false;
 
 void setup() {
-  for (int i=0; i<CLASSES; ++i) pinMode(pins[i], OUTPUT);
+  for (int i=0; i<CLASSES; ++i) {
+    pinMode(pins[i], OUTPUT);
+    analogWrite(pins[i], 0);
+  }
   pinMode(LAMP, OUTPUT);
   pinMode(CONTROL, INPUT_PULLUP);
   pinMode(SWITCH,  INPUT_PULLUP);
@@ -38,13 +43,13 @@ void setup() {
 
 void setLeds(int* leds) {
   for (int i=0; i<CLASSES; ++i) analogWrite(pins[i], leds[i]);
-  Serial.println(leds[0]);
 }
 
 float getFactor() {
-  float factor(analogRead(CONTROL)*1.0/900.0);
+  int ifactor(analogRead(CONTROL));
+  float factor(ifactor / 800.0);
   if (factor>1) factor=1;
-  return factor;
+  if (factor<0.3) factor=0.3  return factor;
 }
 
 void loop() {
@@ -59,13 +64,13 @@ void loop() {
     case MODE_STAR:
     case MODE_TWINKLE: {
       float factor(getFactor());
-      for (int i=0; i<CLASSES; ++i) current[i] = brightness[i] * factor;
+      for (int i=0; i<CLASSES; ++i) current[i] = (float)brightness[i] * factor;
   
       if (MODE_TWINKLE == mode) {
         for (int i=0; i<CLASSES; ++i) {
-          int value(current[i] + random(-TWINKLE, TWINKLE));
+          int value(current[i] * (95 + random(0, 2*TWINKLE)) / 100.0);
           if (value<TWINKLE) value=TWINKLE;
-          if (value>255) value=255;
+          if (value>MAXVALUE) value=MAXVALUE;
           current[i]=value;
         }
       }
@@ -80,14 +85,6 @@ void loop() {
       analogWrite(LAMP, analogRead(CONTROL)/4);
       break;
     }
-    
-    case MODE_FULL_LANP: {
-      float factor(getFactor());
-      for (int i=0; i<CLASSES; ++i) current[i] = 255.0 * factor;
-      setLeds(current);
-      analogWrite(LAMP, 255.0 * factor);
-      break;
-    }
   }
 
   bool button(digitalRead(SWITCH) == LOW);
@@ -100,5 +97,5 @@ void loop() {
 
   prevStatus = button;
 
-  Serial.println(mode);
+  delay(25);
 }
